f11676335fc7b6e5916f5a1d8b560e6555c567b0
Benjie Gillam <benjie@jemjie.com>
2014-06-08 20:37:48 +0100

    Step by step graceful=fs server files.

---
+++ server/000190.js
@@ -0,0 +1,19 @@
+var http = require('http');
+//var fs = require('fs');
+var fs = require('graceful-fs'); // npm install graceful-fs
+
+function listener(request, response) {
+  function send(err, contents) {
+    if (err) {
+      console.error(err); // DON'T DO THIS IN PRODUCTION
+      response.writeHead(500);
+      response.end("ERROR");
+    } else {
+      response.end(contents);
+    }
+  }
+  fs.readFile(require.main.filename, send);
+}
+
+var server = http.createServer(listener);
+server.listen(1337);
